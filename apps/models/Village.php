7287ac43927d69c08a33ca5145cec03850b1a04a
Roy Situmorang <roy.situmorang@gmail.com>
2016-11-23 18:29:31 +0700

    Add model Village.

---
+++ apps/models/Village.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Models;
+
+class Village extends BaseModel {
+	public $id;
+	public $subdistrict_id;
+	public $name;
+	public $zip_code;
+	public $created_by;
+	public $created_at;
+	public $updated_by;
+	public $updated_at;
+
+	function getSource() {
+		return 'villages';
+	}
+
+	function initialize() {
+		parent::initialize();
+		$this->belongsTo('subdistrict_id', 'Application\Models\Subdistrict', 'id', [
+			'alias'    => 'subdistrict',
+			'reusable' => true,
+		]);
+	}
+}
