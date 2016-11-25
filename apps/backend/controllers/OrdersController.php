a964d857e74923aedc70382cb3d1599f4a95eacf
Roy Situmorang <roy.situmorang@gmail.com>
2016-11-25 20:31:27 +0700

    Fix order search conditions.

--- apps/backend/controllers/OrdersController.php
+++ apps/backend/controllers/OrdersController.php
@@ -10,13 +10,22 @@ class OrdersController extends BaseController {
 		$limit          = $this->config->per_page;
 		$current_page   = $this->dispatcher->getParam('page', 'int') ?: 1;
 		$offset         = ($current_page - 1) * $limit;
+		$parameters     = [];
+		$conditions     = [];
 		$status         = Order::STATUS;
 		$current_status = $this->request->getQuery('status', 'int');
-		if ($current_status && !array_key_exists($current_status, $status)) {
-			$current_status = null;
+		if ($current_status && array_key_exists($current_status, $status)) {
+			$conditions[] = "status = {$current_status}";
 		}
+		if ($code = $this->request->getQuery('code', 'int')) {
+			$conditions[] = "code = {$code}";
+		}
+		if ($conditions) {
+			$parameters[] = implode(' AND ', $conditions);
+		}
+		$parameters['order'] = 'id DESC';
 		$paginator = new PaginatorModel([
-			'data'  => Order::find(),
+			'data'  => Order::find($parameters),
 			'limit' => $limit,
 			'page'  => $current_page,
 		]);
@@ -34,6 +43,7 @@ class OrdersController extends BaseController {
 		$this->view->page                   = $paginator->getPaginate();
 		$this->view->status                 = $status;
 		$this->view->current_status         = $current_status;
+		$this->view->code                   = $code;
 		$this->view->total_orders           = $this->db->fetchColumn('SELECT COUNT(1) FROM orders');
 		$this->view->pending_orders         = $this->db->fetchOne("SELECT COUNT(1) AS total, COALESCE(SUM(final_bill), 0) AS bill FROM orders WHERE `status` = 0");
 		$this->view->completed_orders       = $this->db->fetchOne("SELECT COUNT(1) AS total, COALESCE(SUM(final_bill), 0) AS bill FROM orders WHERE `status` = 1");
