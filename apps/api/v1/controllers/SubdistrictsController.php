8a9f436453fc7ab4b40692eab1c1f530285e3cb9
Roy Situmorang <roy.situmorang@gmail.com>
2016-12-09 18:07:59 +0700

    Add API subdistricts.

---
+++ apps/api/v1/controllers/SubdistrictsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Api\V1\Controllers;
+
+use Application\Models\City;
+
+class SubdistrictsController extends ControllerBase {
+	function indexAction() {
+		$this->_response['status'] = 1;
+		$subdistricts              = [];
+		$city                      = City::findFirstByName('Medan');
+		foreach ($city->subdistricts as $subdistrict) {
+			$villages = [];
+			foreach ($subdistrict->villages as $village) {
+				$villages[$village->id] = $village->name;
+			}
+			$subdistricts[$subdistrict->id] = [
+				'name'     => $subdistrict->name,
+				'villages' => $villages,
+			];
+		}
+		$this->_response['data']['subdistricts'] = $subdistricts;
+		$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK);
+		return $this->response;
+	}
+}
