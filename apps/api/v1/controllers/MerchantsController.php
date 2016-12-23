9f085107c1acc46bbbb39acf87c11f75d1402a4b
Roy Situmorang <roy.situmorang@gmail.com>
2016-12-23 07:19:52 +0700

    Mark unavailable merchant.

--- apps/api/v1/controllers/MerchantsController.php
+++ apps/api/v1/controllers/MerchantsController.php
@@ -7,10 +7,12 @@ use Phalcon\Db;
 
 class MerchantsController extends ControllerBase {
 	function indexAction() {
-		$merchants = [];
-		$coupons   = [];
+		$current_day = $this->currentDatetime->format('w');
+		$merchants   = [];
+		$coupons     = [];
 		foreach ($this->db->fetchAll("SELECT a.id, a.company, business_days, business_opening_hour, business_closing_hour FROM users a JOIN service_areas b ON a.id = b.user_id WHERE b.village_id = {$this->_current_user->village_id}", Db::FETCH_OBJ) as $merchant) {
-			$categories = [];
+			$categories    = [];
+			$business_days = json_decode($merchant->business_days);
 			foreach ($this->db->fetchAll("SELECT c.id, c.name FROM product_prices a JOIN products b ON a.product_id = b.id JOIN product_categories c ON b.product_category_id = c.id WHERE a.user_id = {$merchant->id} AND a.published = 1 AND b.published = 1 AND c.published = 1 GROUP BY c.id", Db::FETCH_OBJ) as $category) {
 				$products = [];
 				foreach ($this->db->fetchAll("SELECT a.id, b.name, a.value, b.stock_unit, order_closing_hour FROM product_prices a JOIN products b ON a.product_id = b.id JOIN product_categories c ON b.product_category_id = c.id WHERE a.user_id = {$merchant->id} AND a.published = 1 AND b.published = 1 AND c.published = 1 AND c.id = {$category->id} GROUP BY b.id", Db::FETCH_OBJ) as $product) {
@@ -30,11 +32,14 @@ class MerchantsController extends ControllerBase {
 			}
 			$merchants[$merchant->id] = [
 				'company'               => $merchant->company,
-				'business_days'         => json_decode($merchant->business_days),
+				'business_days'         => $business_days,
 				'business_opening_hour' => $merchant->business_opening_hour,
 				'business_closing_hour' => $merchant->business_closing_hour,
 				'categories'            => $categories,
 			];
+			if (!in_array($current_day, $business_days)) {
+				$merchants[$merchant->id]['unavailable'] = 1;
+			}
 		}
 		$this->_response['status'] = 1;
 		if (!$merchants) {
