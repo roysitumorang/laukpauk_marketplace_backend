fd05d159ab10b4f69c00b2e4484c3b20539236dd
Roy Situmorang <roy.situmorang@gmail.com>
2017-01-23 16:31:19 +0700

    Add posts API.

---
+++ apps/api/v1/controllers/PostsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Api\V1\Controllers;
+
+use Application\Models\PostCategory;
+use Exception;
+
+class PostsController extends ControllerBase {
+	function showAction($permalink) {
+		try {
+			$category = PostCategory::findFirst("published = 1 AND name = 'App Info'");
+			if (!$category) {
+				throw new Exception('Konten tidak ditemukan!');
+			}
+			$post = $category->getPosts(['published = 1 AND permalink = ?0', 'bind' => [$permalink]])->getFirst();
+			if (!$post) {
+				throw new Exception('Konten tidak ditemukan!');
+			}
+			$this->_response['status'] = 1;
+			$this->_response['data']   = [
+				'subject' => $post->subject,
+				'body'    => $post->body,
+			];
+		} catch (Exception $e) {
+			$this->_response['message'] = $e->getMessage();
+		}
+		$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
+		return $this->response;
+	}
+}
