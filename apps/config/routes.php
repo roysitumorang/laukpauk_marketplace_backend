da2c19ee9cb7c33d19e22a2d9ec69ab8cb2fc1f8
Roy Situmorang <roy.situmorang@gmail.com>
2017-03-02 21:54:12 +0700

    Add custom merchants#show route.

--- apps/config/routes.php
+++ apps/config/routes.php
@@ -59,6 +59,14 @@ $di->set('router', function() {
 		'params'         => 3,
 	]);
 
+	$router->add('/api/v2(/([a-z0-9]{32}))?/merchants/:int', [
+		'module'         => 'v2',
+		'merchant_token' => 2,
+		'controller'     => 'merchants',
+		'action'         => 'show',
+		'params'         => 3,
+	]);
+
 	$router->add('/api/v2(/([a-z0-9]{32}))?/:controller', [
 		'module'         => 'v2',
 		'merchant_token' => 2,
