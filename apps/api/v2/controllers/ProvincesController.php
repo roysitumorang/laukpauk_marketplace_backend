2ea139cc6a6282f96a8e5c1b354bd9b9f392b773
Roy Situmorang <roy.situmorang@gmail.com>
2017-03-03 02:17:15 +0700

    Separate condition for premium and free merchant service areas.

--- apps/api/v2/controllers/ProvincesController.php
+++ apps/api/v2/controllers/ProvincesController.php
@@ -10,7 +10,35 @@ class ProvincesController extends ControllerBase {
 	function indexAction() {
 		if (!$this->cache->exists('provinces')) {
 			$provinces = [];
-			$result    = $this->db->query("SELECT a.id AS province_id, a.name AS province_name, b.id AS city_id, CONCAT_WS(' ', b.type, b.name) AS city_name, c.id AS subdistrict_id, c.name AS subdistrict_name, d.id AS village_id, d.name AS village_name FROM provinces a JOIN cities b ON a.id = b.province_id JOIN subdistricts c ON b.id = c.city_id JOIN villages d ON c.id = d.subdistrict_id WHERE EXISTS(SELECT 1 FROM service_areas e WHERE e.village_id = d.id) ORDER BY province_name, city_name, subdistrict_name, village_name");
+			$query     = <<<QUERY
+				SELECT
+					a.id AS province_id,
+					a.name AS province_name,
+					b.id AS city_id,
+					CONCAT_WS(' ', b.type, b.name) AS city_name,
+					c.id AS subdistrict_id,
+					c.name AS subdistrict_name,
+					d.id AS village_id,
+					d.name AS village_name
+				FROM
+					provinces a
+					JOIN cities b ON a.id = b.province_id
+					JOIN subdistricts c ON b.id = c.city_id
+					JOIN villages d ON c.id = d.subdistrict_id
+				WHERE
+					EXISTS(SELECT 1 FROM service_areas e WHERE e.village_id = d.id
+QUERY;
+			if ($this->_premium_merchant) {
+				$query .= " AND e.user_id = {$this->_premium_merchant->id}";
+			}
+			$query .= <<<QUERY
+				) ORDER BY
+					province_name,
+					city_name,
+					subdistrict_name,
+					village_name
+QUERY;
+			$result = $this->db->query($query);
 			$result->setFetchMode(Db::FETCH_OBJ);
 			while ($row = $result->fetch()) {
 				if (!isset($provinces[$row->province_id])) {
