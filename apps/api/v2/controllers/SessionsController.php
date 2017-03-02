a86a66a7cd77a007e0cfec792bcfc25801981719
Roy Situmorang <roy.situmorang@gmail.com>
2017-03-02 10:35:28 +0700

    Fix premium merchant login.

--- apps/api/v2/controllers/SessionsController.php
+++ apps/api/v2/controllers/SessionsController.php
@@ -46,10 +46,14 @@ class SessionsController extends ControllerBase {
 			$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
 			return $this->response;
 		}
-		$params = $premium_merchant
-			? ['status = 1 AND mobile_phone = ?0 AND ((role_id = ?1 AND merchant_id = ?2) OR (role_id = ?3 AND merchant_id = ?4))', 'bind' => [$this->_input->mobile_phone, Role::MERCHANT, $premium_merchant->id, Role::BUYER, $premium_merchant->id]]
-			: ['status = 1 AND merchant_token IS NULL AND merchant_id IS NULL AND role_id IN ({role_ids:array}) AND mobile_phone = :mobile_phone:', 'bind' => ['role_ids' => [Role::BUYER, Role::MERCHANT], 'mobile_phone' => $this->_input->mobile_phone]];
-		$user = User::findFirst($params);
+		if ($premium_merchant && $premium_merchant->mobile_phone == $this->_input->mobile_phone) {
+			$user = $premium_merchant;
+		} else {
+			$params = $premium_merchant
+				? ['status = 1 AND mobile_phone = ?0 AND ((role_id = ?1 AND merchant_id = ?2) OR (role_id = ?3 AND merchant_id = ?4))', 'bind' => [$this->_input->mobile_phone, Role::MERCHANT, $premium_merchant->id, Role::BUYER, $premium_merchant->id]]
+				: ['status = 1 AND merchant_token IS NULL AND merchant_id IS NULL AND role_id IN ({role_ids:array}) AND mobile_phone = :mobile_phone:', 'bind' => ['role_ids' => [Role::BUYER, Role::MERCHANT], 'mobile_phone' => $this->_input->mobile_phone]];
+			$user = User::findFirst($params);
+		}
 		if (!$user || !$this->security->checkHash($this->_input->password, $user->password)) {
 			$this->_response['message'] = 'Nomor HP dan/atau password salah!';
 			$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
