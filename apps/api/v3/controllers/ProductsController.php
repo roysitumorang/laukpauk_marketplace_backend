0cfda2da0731b8618a5ed40063cb1341c213ea05
Roy Situmorang <roy.situmorang@gmail.com>
2017-05-16 00:51:57 +0700

    Merchant and category are optional parameters for products API.

--- apps/api/v3/controllers/ProductsController.php
+++ apps/api/v3/controllers/ProductsController.php
@@ -2,100 +2,87 @@
 
 namespace Application\Api\V3\Controllers;
 
-use Exception;
 use Phalcon\Db;
 
 class ProductsController extends ControllerBase {
 	function indexAction() {
-		$merchant_id = $this->dispatcher->getParam('merchant_id');
-		try {
-			if (!filter_var($merchant_id, FILTER_VALIDATE_INT)) {
-				throw new Exception('Merchant tidak valid!');
-			}
-			$query = <<<QUERY
-				SELECT
-					a.id,
-					a.merchant_note,
-					COALESCE(c.minimum_purchase, a.minimum_purchase, d.value) AS minimum_purchase,
-					a.shipping_cost
-				FROM
-					users a
-					JOIN roles b ON a.role_id = b.id
-					JOIN service_areas c ON a.id = c.user_id
-					JOIN settings d ON d.name = 'minimum_purchase'
-				WHERE
-					a.status = 1 AND
-					b.name = 'Merchant' AND
-					c.village_id = {$this->_current_user->village->id} AND
-QUERY;
-			if ($this->_premium_merchant) {
-				$query .= <<<QUERY
-					a.premium_merchant = 1 AND
-					a.id = {$this->_premium_merchant->id}
-QUERY;
-			} else {
-				$query .= <<<QUERY
-					a.premium_merchant IS NULL AND
-					a.id = {$merchant_id}
-QUERY;
-			}
-			$merchant = $this->db->fetchOne($query, Db::FETCH_OBJ);
-			if (!$merchant) {
-				throw new Exception('Merchant tidak valid!');
-			}
-		} catch (Exception $e) {
-			$this->_response['message'] = $e->getMessage();
-			$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK);
-			return $this->response;
-		}
-		$category_id = $this->dispatcher->getParam('category_id', 'int');
-		$page        = $this->dispatcher->getParam('page', 'int');
-		$keyword     = $this->dispatcher->getParam('keyword', 'string');
-		$limit       = 10;
-		$products    = [];
-		$query       = <<<QUERY
+		$merchant_id  = $this->dispatcher->getParam('merchant_id', 'int');
+		$category_id  = $this->dispatcher->getParam('category_id', 'int');
+		$page         = $this->dispatcher->getParam('page', 'int');
+		$search_query = $this->dispatcher->getParam('keyword', 'string') ?: null;
+		$limit        = 10;
+		$params       = [];
+		$products     = [];
+		$query        = <<<QUERY
 			SELECT
-				COUNT(1)
+				COUNT(DISTINCT f.id)
 			FROM
-				store_items a
-				JOIN products b ON a.product_id = b.id
-				JOIN product_categories c ON b.product_category_id = c.id
+				users a
+				JOIN roles b ON a.role_id = b.id
+				JOIN service_areas c ON a.id = c.user_id
+				JOIN settings d ON d.name = 'minimum_purchase'
+				JOIN store_items e ON a.id = e.user_id
+				JOIN products f ON e.product_id = f.id
+				JOIN product_categories g ON f.product_category_id = g.id
 			WHERE
-				a.published = 1 AND
-				b.published = 1 AND
-				c.published = 1 AND
+				a.status = 1 AND
+				b.name = 'Merchant' AND
+				c.village_id = {$this->_current_user->village->id} AND
+				e.published = 1 AND
+				f.published = 1 AND
+				g.published = 1 AND
+				a.premium_merchant
 QUERY;
 		if ($this->_premium_merchant) {
-			$query .= " a.user_id = {$this->_premium_merchant->id}";
+			$query .= " = 1 AND a.id = {$this->_premium_merchant->id}";
 		} else {
-			$query .= " a.user_id = {$merchant->id}";
+			$query .= ' IS NULL';
+			if ($merchant_id) {
+				$query .= " AND a.id = {$merchant_id}";
+			}
+			if ($search_query) {
+				$stop_words            = preg_split('/,/', $this->db->fetchColumn("SELECT value FROM settings WHERE name = 'stop_words'"), -1, PREG_SPLIT_NO_EMPTY);
+				$keywords              = preg_split('/ /', strtolower($search_query), -1, PREG_SPLIT_NO_EMPTY);
+				$filtered_keywords     = array_diff($keywords, $stop_words);
+				$filtered_search_query = implode(' ', $filtered_keywords);
+				$query                .= ' AND (a.company LIKE ? OR f.name LIKE ? OR g.name LIKE ?';
+				foreach (range(1, 3) as $i) {
+					$params[] = "%{$filtered_search_query}%";
+				}
+				if (count($filtered_keywords) > 1) {
+					foreach ($filtered_keywords as $keyword) {
+						$query .= ' OR a.company LIKE ? OR f.name LIKE ? OR g.name LIKE ?';
+						foreach (range(1, 3) as $i) {
+							$params[] = "%{$keyword}%";
+						}
+					}
+				}
+				$query .= ')';
+			}
 		}
-		if ($category_id && $category = $this->db->fetchOne("SELECT id FROM product_categories WHERE id = {$category_id}", Db::FETCH_OBJ)) {
-			$query .= " AND c.id = {$category->id}";
+		if ($category_id) {
+			$query .= " AND g.id = {$category_id}";
 		}
 		if ($keyword) {
-			$query .= " AND b.name LIKE '%{$keyword}%'";
+			$query .= " AND f.name LIKE '%{$keyword}%'";
 		}
-		$total_products   = $this->db->fetchColumn($query);
+		$total_products   = $this->db->fetchColumn($query, $params);
 		$total_pages      = ceil($total_products / $limit);
 		$current_page     = $page > 0 && $page <= $total_pages ? $page : 1;
 		$offset           = ($current_page - 1) * $limit;
-		$result           = $this->db->query(str_replace('COUNT(1)', 'a.id, b.product_category_id, b.name, a.price, a.stock, b.stock_unit, order_closing_hour, b.picture', $query) . " GROUP BY b.id ORDER BY b.name LIMIT {$limit} OFFSET {$offset}");
+		$result           = $this->db->query(str_replace('COUNT(DISTINCT f.id)', 'f.id, f.name, e.price, e.stock, f.stock_unit, e.order_closing_hour, f.picture', $query) . " GROUP BY f.id ORDER BY f.name LIMIT {$limit} OFFSET {$offset}", $params);
 		$picture_root_url = 'http' . ($this->request->getScheme() === 'https' ? 's' : '') . '://' . $this->request->getHttpHost() . '/assets/image/';
 		$result->setFetchMode(Db::FETCH_OBJ);
 		while ($row = $result->fetch()) {
-			$product = [
-				'id'         => $row->id,
-				'name'       => $row->name,
-				'price'      => $row->price,
-				'stock'      => $row->stock,
-				'stock_unit' => $row->stock_unit,
-				'picture'    => $row->picture ? $picture_root_url . strtr($row->picture, ['.jpg' => '120.jpg']) : null,
-			];
-			if ($row->order_closing_hour) {
-				$product['order_closing_hour'] = $row->order_closing_hour;
+			if ($row->picture) {
+				$row->picture = $picture_root_url . strtr($row->picture, ['.jpg' => '120.jpg']);
+			} else {
+				unset($row->picture);
 			}
-			$products[] = $product;
+			if (!$row->order_closing_hour) {
+				unset($row->order_closing_hour);
+			}
+			$products[] = $row;
 		}
 		if (!$total_products) {
 			$this->_response['message'] = $keyword ? 'Produk tidak ditemukan.' : 'Produk belum ada.';
@@ -103,14 +90,11 @@ QUERY;
 			$this->_response['status'] = 1;
 		}
 		$this->_response['data'] = [
-			'merchant_note'    => $merchant->merchant_note,
-			'products'         => $products,
-			'total_pages'      => $total_pages,
-			'current_page'     => $current_page,
-			'current_hour'     => $this->currentDatetime->format('G'),
-			'minimum_purchase' => $merchant->minimum_purchase,
-			'shipping_cost'    => $merchant->shipping_cost ?? 0,
-			'pages'            => $this->_setPaginationRange($total_pages, $current_page),
+			'products'     => $products,
+			'total_pages'  => $total_pages,
+			'current_page' => $current_page,
+			'current_hour' => $this->currentDatetime->format('G'),
+			'pages'        => $this->_setPaginationRange($total_pages, $current_page),
 		];
 		$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
 		return $this->response;
