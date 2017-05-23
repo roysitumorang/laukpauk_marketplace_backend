8016586fbb485cc76ff836cf04776e2040d678d0
Roy Situmorang <roy.situmorang@gmail.com>
2017-05-23 08:01:19 +0700

    Remove Mysql backticks.

--- apps/api/v2/controllers/ControllerBase.php
+++ apps/api/v2/controllers/ControllerBase.php
@@ -32,7 +32,7 @@ abstract class ControllerBase extends Controller {
 				'created_at'     => $this->currentDatetime->format('Y-m-d H:i:s'),
 			]);
 		});
-		$this->_response['version'] = $this->db->fetchColumn('SELECT MAX(`version`) FROM releases');
+		$this->_response['version'] = $this->db->fetchColumn('SELECT MAX(version) FROM releases');
 		if (Setting::findFirstByName('maintenance_mode')->value) {
 			$this->_response['maintenance_mode'] = 1;
 			$this->response->setJsonContent($this->_response);
