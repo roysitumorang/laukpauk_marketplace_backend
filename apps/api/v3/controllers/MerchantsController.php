5fcb07fba5f1a08d61d57c634320fa34f5714270
Roy Situmorang <roy.situmorang@gmail.com>
2017-05-26 15:47:10 +0700

    Remove categories on Merchants API, remove method show, add delivery days on method index.

--- apps/api/v3/controllers/MerchantsController.php
+++ apps/api/v3/controllers/MerchantsController.php
@@ -3,11 +3,9 @@
 namespace Application\Api\V3\Controllers;
 
 use Application\Models\Post;
-use Application\Models\ProductCategory;
 use Application\Models\User;
 use Application\Models\Role;
 use DateTime;
-use Error;
 use IntlDateFormatter;
 use Phalcon\Db;
 
@@ -19,12 +17,21 @@ class MerchantsController extends ControllerBase {
 	}
 
 	function indexAction() {
-		$page         = $this->dispatcher->getParam('page', 'int');
-		$search_query = $this->dispatcher->getParam('keyword', 'string') ?: null;
-		$limit        = 10;
-		$merchants    = [];
-		$params       = [];
-		$query        = <<<QUERY
+		$page           = $this->dispatcher->getParam('page', 'int');
+		$search_query   = $this->dispatcher->getParam('keyword', 'string') ?: null;
+		$limit          = 10;
+		$merchants      = [];
+		$params         = [];
+		$day_aliases    = ['Hari ini', 'Besok', 'Lusa', '3 hari lagi', '4 hari lagi', '5 hari lagi', '6 hari lagi'];
+		$date_formatter = new IntlDateFormatter(
+			'id_ID',
+			IntlDateFormatter::FULL,
+			IntlDateFormatter::NONE,
+			$this->currentDatetime->getTimezone(),
+			IntlDateFormatter::GREGORIAN,
+			'EEEE, d MMM yyyy'
+		);
+		$query = <<<QUERY
 			SELECT
 				COUNT(DISTINCT a.id)
 			FROM
@@ -70,6 +77,8 @@ QUERY;
 		$result          = $this->db->query(str_replace('COUNT(DISTINCT a.id)', 'DISTINCT a.id, a.company, a.address, a.open_on_sunday, a.open_on_monday, a.open_on_tuesday, a.open_on_wednesday, a.open_on_thursday, a.open_on_friday, a.open_on_saturday, a.business_opening_hour, a.business_closing_hour, a.delivery_hours, COALESCE(c.minimum_purchase, a.minimum_purchase, d.value::INT) AS minimum_purchase, a.shipping_cost', $query) . " ORDER BY a.company LIMIT {$limit} OFFSET {$offset}", $params);
 		$result->setFetchMode(Db::FETCH_OBJ);
 		while ($item = $result->fetch()) {
+			$delivery_days = [];
+			$now           = (new DateTime(null, $this->currentDatetime->getTimezone()))->setTimestamp($this->currentDatetime->getTimestamp());
 			$business_days = [
 				$item->open_on_monday    ? 'Senin'  : ',',
 				$item->open_on_tuesday   ? 'Selasa' : ',',
@@ -80,6 +89,7 @@ QUERY;
 				$item->open_on_sunday    ? 'Minggu' : ',',
 			];
 			$business_hours = range($item->business_opening_hour, $item->business_closing_hour);
+			$delivery_hours = $item->delivery_hours ? explode(',', $item->delivery_hours) : $business_hours;
 			if ($hours = explode(',', $item->delivery_hours)) {
 				foreach ($business_hours as &$hour) {
 					if (!in_array($hour, $hours)) {
@@ -89,156 +99,57 @@ QUERY;
 					}
 				}
 			}
-			$delivery_hours = trim(preg_replace(['/\,+/', '/(0)([1-9])/', '/([1-2]?[0-9]\.00)(-[1-2]?[0-9]\.00)+(-[1-2]?[0-9]\.00)/'], [',', '\1-\2', '\1\3'], implode('', $business_hours)), ',');
-			$merchant       = [
-				'id'                    => $item->id,
-				'company'               => $item->company,
-				'address'               => $item->address,
-				'business_days'         => trim(preg_replace(['/\,+/', '/([a-z])([A-Z])/', '/([A-Za-z]+)(-[A-Za-z]+)+(-[A-Za-z]+)/'], [',', '\1-\2', '\1\3'], implode('', $business_days)), ',') ?: '-',
-				'business_opening_hour' => $item->business_opening_hour . '.00',
-				'business_closing_hour' => $item->business_closing_hour . '.00 WIB',
-				'delivery_hours'        => $delivery_hours ? $delivery_hours . ' WIB' : '-',
-				'minimum_purchase'      => $item->minimum_purchase,
-				'shipping_cost'         => $item->shipping_cost ?? 0,
+			$stringified_delivery_hours = trim(preg_replace(['/\,+/', '/(0)([1-9])/', '/([1-2]?[0-9]\.00)(-[1-2]?[0-9]\.00)+(-[1-2]?[0-9]\.00)/'], [',', '\1-\2', '\1\3'], implode('', $business_hours)), ',');
+			foreach ($day_aliases as $i => $alias) {
+				$i && $now->modify('+1 day');
+				$current_hour = $now->format('G');
+				if (!$i && $current_hour >= max($delivery_hours)) {
+					continue;
+				}
+				$current_day  = $now->format('l');
+				$delivery_day = [
+					'date'  => $now->format('Y-m-d'),
+					'label' => $date_formatter->format($now) . ' (' . $alias . ')',
+				];
+				if (($current_day == 'Sunday' && !$item->open_on_sunday) ||
+					($current_day == 'Monday' && !$item->open_on_monday) ||
+					($current_day == 'Tuesday' && !$item->open_on_tuesday) ||
+					($current_day == 'Wednesday' && !$item->open_on_wednesday) ||
+					($current_day == 'Thursday' && !$item->open_on_thursday) ||
+					($current_day == 'Friday' && !$item->open_on_friday) ||
+					($current_day == 'Saturday' && !$item->open_on_saturday)) {
+					$delivery_day['unavailable'] = true;
+				} else {
+					$minimum_hour          = $current_hour + ($now->format('i') > 29 ? 2 : 1);
+					$delivery_day['hours'] = !$i
+						? array_values(array_filter($delivery_hours, function($v, $k) use($minimum_hour) {
+							return $v >= $minimum_hour;
+						}, ARRAY_FILTER_USE_BOTH))
+						: $delivery_hours;
+				}
+				$delivery_days[] = $delivery_day;
+			}
+			$merchants[] = [
+				'id'                         => $item->id,
+				'company'                    => $item->company,
+				'address'                    => $item->address,
+				'business_days'              => trim(preg_replace(['/\,+/', '/([a-z])([A-Z])/', '/([A-Za-z]+)(-[A-Za-z]+)+(-[A-Za-z]+)/'], [',', '\1-\2', '\1\3'], implode('', $business_days)), ',') ?: '-',
+				'business_opening_hour'      => $item->business_opening_hour . '.00',
+				'business_closing_hour'      => $item->business_closing_hour . '.00 WIB',
+				'stringified_delivery_hours' => $stringified_delivery_hours ? $stringified_delivery_hours . ' WIB' : '-',
+				'minimum_purchase'           => $item->minimum_purchase,
+				'shipping_cost'              => $item->shipping_cost ?? 0,
+				'delivery_hours'             => $delivery_hours,
+				'delivery_days'              => $delivery_days,
 			];
-			$merchants[] = $merchant;
 		}
 		if (!$merchants) {
 			$this->_response['message'] = $search_query ? 'Penjual tidak ditemukan.' : 'Maaf, daerah Anda di luar wilayah operasional Kami.';
 		} else {
-			$categories = ProductCategory::find([
-				'conditions' => 'published = 1',
-				'columns'    => 'id, name',
-				'order'      => 'name',
-			])->toArray();
-			$this->_response['status'] = 1;
-			$this->_response['data']   = [
-				'merchants'    => $merchants,
-				'categories'   => $categories,
-				'current_page' => $current_page,
-				'pages'        => $this->_setPaginationRange($total_pages, $current_page),
-			];
+			$this->_response['status']            = 1;
+			$this->_response['data']['merchants'] = $merchants;
 		}
 		$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
-		return $this->response;
-	}
-
-	function showAction($id) {
-		try {
-			if (!filter_var($id, FILTER_VALIDATE_INT)) {
-				throw new Error('Merchant tidak valid!');
-			}
-			$query = <<<QUERY
-				SELECT
-					a.id,
-					a.company,
-					a.open_on_sunday,
-					a.open_on_monday,
-					a.open_on_tuesday,
-					a.open_on_wednesday,
-					a.open_on_thursday,
-					a.open_on_friday,
-					a.open_on_saturday,
-					a.business_opening_hour,
-					a.business_closing_hour,
-					a.delivery_hours
-				FROM
-					users a
-					JOIN roles b ON a.role_id = b.id
-					JOIN service_areas c ON a.id = c.user_id
-				WHERE
-					a.status = 1 AND
-					b.name = 'Merchant' AND
-					c.village_id = {$this->_current_user->village->id} AND
-QUERY;
-			if ($this->_premium_merchant) {
-				$query .= <<<QUERY
-					a.premium_merchant = 1 AND
-					a.id = {$this->_premium_merchant->id}
-QUERY;
-			} else {
-				$query .= <<<QUERY
-					a.premium_merchant IS NULL AND
-					a.id = {$id}
-QUERY;
-			}
-			$merchant = $this->db->fetchOne($query, Db::FETCH_OBJ);
-			if (!$merchant) {
-				throw new Error('Merchant tidak valid!');
-			}
-		} catch (Throwable $e) {
-			$this->_response['message'] = $e->getMessage();
-			$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK);
-			return $this->response;
-		}
-		$delivery_days  = [];
-		$categories     = [];
-		$now            = (new DateTime(null, $this->currentDatetime->getTimezone()))->setTimestamp($this->currentDatetime->getTimestamp());
-		$date_formatter = new IntlDateFormatter(
-			'id_ID',
-			IntlDateFormatter::FULL,
-			IntlDateFormatter::NONE,
-			$this->currentDatetime->getTimezone(),
-			IntlDateFormatter::GREGORIAN,
-			'EEEE, d MMM yyyy'
-		);
-		if ($merchant->delivery_hours) {
-			$merchant->delivery_hours = explode(',', $merchant->delivery_hours);
-		}
-		$delivery_hours = $merchant->delivery_hours ?: range($merchant->business_opening_hour, $merchant->business_closing_hour);
-		for ($i = 0; $i < 7; $i++) {
-			$i && $now->modify('+1 day');
-			$current_hour = $now->format('G');
-			if (!$i && $current_hour >= max($delivery_hours)) {
-				continue;
-			}
-			$current_day  = $now->format('l');
-			$delivery_day = [
-				'date'  => $now->format('Y-m-d'),
-				'label' => $date_formatter->format($now),
-			];
-			if (($current_day == 'Sunday' && !$merchant->open_on_sunday) ||
-				($current_day == 'Monday' && !$merchant->open_on_monday) ||
-				($current_day == 'Tuesday' && !$merchant->open_on_tuesday) ||
-				($current_day == 'Wednesday' && !$merchant->open_on_wednesday) ||
-				($current_day == 'Thursday' && !$merchant->open_on_thursday) ||
-				($current_day == 'Friday' && !$merchant->open_on_friday) ||
-				($current_day == 'Saturday' && !$merchant->open_on_saturday)) {
-				$delivery_day['unavailable'] = true;
-			} else {
-				$minimum_hour          = $current_hour + ($now->format('i') > 29 ? 2 : 1);
-				$delivery_day['hours'] = !$i
-					? array_values(array_filter($delivery_hours, function($v, $k) use($minimum_hour) {
-						return $v >= $minimum_hour;
-					}, ARRAY_FILTER_USE_BOTH))
-					: $delivery_hours;
-			}
-			$delivery_days[] = $delivery_day;
-		}
-		$result = $this->db->query(<<<QUERY
-			SELECT
-				b.id,
-				b.name
-			FROM
-				products a
-				JOIN product_categories b ON a.product_category_id = b.id
-			WHERE
-				a.user_id = {$merchant->id} AND
-				a.price > 0 AND
-				a.published = 1 AND
-				b.published = 1
-			GROUP BY b.id
-			ORDER BY b.name
-QUERY
-		);
-		$result->setFetchMode(Db::FETCH_OBJ);
-		while ($item = $result->fetch()) {
-			$categories[] = $item;
-		}
-		$this->_response['status']                = 1;
-		$this->_response['data']['delivery_days'] = $delivery_days;
-		$this->_response['data']['categories']    = $categories;
-		$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK);
 		return $this->response;
 	}
 
