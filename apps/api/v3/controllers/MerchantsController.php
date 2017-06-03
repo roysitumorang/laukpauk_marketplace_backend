00e56a731c2a80b40db6f752ccbb76e13feb4cd2
Roy Situmorang <roy.situmorang@gmail.com>
2017-06-03 12:20:29 +0700

    Remove delivery dates and hours on Merchants API.

--- apps/api/v3/controllers/MerchantsController.php
+++ apps/api/v3/controllers/MerchantsController.php
@@ -5,7 +5,6 @@ namespace Application\Api\V3\Controllers;
 use Application\Models\Post;
 use Application\Models\User;
 use Application\Models\Role;
-use DateTime;
 use IntlDateFormatter;
 use Phalcon\Db;
 use stdClass;
@@ -18,20 +17,12 @@ class MerchantsController extends ControllerBase {
 	}
 
 	function indexAction() {
-		$page           = $this->dispatcher->getParam('page', 'int');
-		$search_query   = $this->dispatcher->getParam('keyword', 'string') ?: null;
-		$limit          = 10;
-		$merchants      = [];
-		$params         = [];
-		$date_formatter = new IntlDateFormatter(
-			'id_ID',
-			IntlDateFormatter::FULL,
-			IntlDateFormatter::NONE,
-			$this->currentDatetime->getTimezone(),
-			IntlDateFormatter::GREGORIAN,
-			'EEEE, d MMM yyyy'
-		);
-		$query = <<<QUERY
+		$page         = $this->dispatcher->getParam('page', 'int');
+		$search_query = $this->dispatcher->getParam('keyword', 'string') ?: null;
+		$limit        = 10;
+		$merchants    = [];
+		$params       = [];
+		$query        = <<<QUERY
 			SELECT
 				COUNT(DISTINCT a.id)
 			FROM
@@ -77,8 +68,6 @@ QUERY;
 		$result          = $this->db->query(str_replace('COUNT(DISTINCT a.id)', 'DISTINCT a.id, a.company, a.address, a.open_on_sunday, a.open_on_monday, a.open_on_tuesday, a.open_on_wednesday, a.open_on_thursday, a.open_on_friday, a.open_on_saturday, a.business_opening_hour, a.business_closing_hour, a.delivery_hours, COALESCE(c.minimum_purchase, a.minimum_purchase, d.value::INT) AS minimum_purchase, a.shipping_cost', $query) . " ORDER BY a.company LIMIT {$limit} OFFSET {$offset}", $params);
 		$result->setFetchMode(Db::FETCH_OBJ);
 		while ($item = $result->fetch()) {
-			$delivery_days = [];
-			$now           = (new DateTime(null, $this->currentDatetime->getTimezone()))->setTimestamp($this->currentDatetime->getTimestamp());
 			$business_days = [
 				$item->open_on_monday    ? 'Senin'  : ',',
 				$item->open_on_tuesday   ? 'Selasa' : ',',
@@ -89,7 +78,6 @@ QUERY;
 				$item->open_on_sunday    ? 'Minggu' : ',',
 			];
 			$business_hours = range($item->business_opening_hour, $item->business_closing_hour);
-			$delivery_hours = $item->delivery_hours ? explode(',', $item->delivery_hours) : $business_hours;
 			if ($hours = explode(',', $item->delivery_hours)) {
 				foreach ($business_hours as &$hour) {
 					if (!in_array($hour, $hours)) {
@@ -99,46 +87,16 @@ QUERY;
 					}
 				}
 			}
-			$stringified_delivery_hours = trim(preg_replace(['/\,+/', '/(0)([1-9])/', '/([1-2]?[0-9]\.00)(-[1-2]?[0-9]\.00)+(-[1-2]?[0-9]\.00)/'], [',', '\1-\2', '\1\3'], implode('', $business_hours)), ',');
-			for ($i = 0; $i < 7; $i++) {
-				$i && $now->modify('+1 day');
-				$current_hour = $now->format('G');
-				if (!$i && $current_hour >= max($delivery_hours)) {
-					continue;
-				}
-				$current_day         = $now->format('l');
-				$delivery_day        = new stdClass;
-				$delivery_day->label = $date_formatter->format($now);
-				if (($current_day == 'Sunday' && !$item->open_on_sunday) ||
-					($current_day == 'Monday' && !$item->open_on_monday) ||
-					($current_day == 'Tuesday' && !$item->open_on_tuesday) ||
-					($current_day == 'Wednesday' && !$item->open_on_wednesday) ||
-					($current_day == 'Thursday' && !$item->open_on_thursday) ||
-					($current_day == 'Friday' && !$item->open_on_friday) ||
-					($current_day == 'Saturday' && !$item->open_on_saturday)) {
-					$delivery_day->unavailable = true;
-				} else {
-					$minimum_hour        = $current_hour + ($now->format('i') > 29 ? 2 : 1);
-					$delivery_day->hours = !$i
-						? array_values(array_filter($delivery_hours, function($v, $k) use($minimum_hour) {
-							return $v >= $minimum_hour;
-						}, ARRAY_FILTER_USE_BOTH))
-						: $delivery_hours;
-				}
-				$delivery_days[$now->format('Y-m-d')] = $delivery_day;
-			}
+			$delivery_hours = trim(preg_replace(['/\,+/', '/(0)([1-9])/', '/([1-2]?[0-9]\.00)(-[1-2]?[0-9]\.00)+(-[1-2]?[0-9]\.00)/'], [',', '\1-\2', '\1\3'], implode('', $business_hours)), ',');
 			$merchants[] = [
-				'id'                         => $item->id,
-				'company'                    => $item->company,
-				'address'                    => $item->address,
-				'business_days'              => trim(preg_replace(['/\,+/', '/([a-z])([A-Z])/', '/([A-Za-z]+)(-[A-Za-z]+)+(-[A-Za-z]+)/'], [',', '\1-\2', '\1\3'], implode('', $business_days)), ',') ?: '-',
-				'business_opening_hour'      => $item->business_opening_hour . '.00',
-				'business_closing_hour'      => $item->business_closing_hour . '.00 WIB',
-				'stringified_delivery_hours' => $stringified_delivery_hours ? $stringified_delivery_hours . ' WIB' : '-',
-				'minimum_purchase'           => $item->minimum_purchase,
-				'shipping_cost'              => $item->shipping_cost ?? 0,
-				'delivery_hours'             => $delivery_hours,
-				'delivery_days'              => $delivery_days,
+				'id'               => $item->id,
+				'company'          => $item->company,
+				'address'          => $item->address,
+				'business_days'    => trim(preg_replace(['/\,+/', '/([a-z])([A-Z])/', '/([A-Za-z]+)(-[A-Za-z]+)+(-[A-Za-z]+)/'], [',', '\1-\2', '\1\3'], implode('', $business_days)), ',') ?: '-',
+				'business_hours'   => $item->business_opening_hour . '.00 - ' . $item->business_closing_hour . '.00 WIB',
+				'delivery_hours'   => $delivery_hours ? $delivery_hours . ' WIB' : '-',
+				'minimum_purchase' => $item->minimum_purchase,
+				'shipping_cost'    => $item->shipping_cost ?? 0,
 			];
 		}
 		if (!$merchants) {
