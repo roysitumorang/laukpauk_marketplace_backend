4527e8686f2accd73a96f1c5a991f917dfa5b52a
Roy Situmorang <roy.situmorang@gmail.com>
2017-06-07 04:26:34 +0700

    Successful coupon validation returns discount.

--- apps/api/v3/controllers/CouponsController.php
+++ apps/api/v3/controllers/CouponsController.php
@@ -8,16 +8,42 @@ use Phalcon\Db;
 
 class CouponsController extends ControllerBase {
 	function validateAction() {
-		$discounts = [];
-		$today     = $this->currentDatetime->format('Y-m-d');
+		$code     = $this->_server->coupon_code;
+		$discount = 0;
+		$today    = $this->currentDatetime->format('Y-m-d');
 		try {
-			if (!$this->request->isOptions() || $this->_current_user->role->name != 'Buyer') {
+			if (!$this->request->isOptions() || $this->_current_user->role->name != 'Buyer' || !$code) {
 				throw new Error('Request tidak valid!');
 			}
+			$params = [<<<QUERY
+				SELECT
+					a.price_discount,
+					a.discount_type,
+					a.multiple_use,
+					a.minimum_purchase,
+					COUNT(1) AS usage
+				FROM
+					coupons a
+					LEFT JOIN orders b ON a.id = b.coupon_id AND b.status = '1' AND b.buyer_id = ?
+				WHERE
+					a.status = '1' AND
+					a.effective_date <= ? AND
+					a.expiry_date > ? AND
+					LOWER(a.code) = ? AND
+					a.user_id
+QUERY
+				,
+				$this->_current_user->id,
+				$today,
+				$today,
+				strtolower($code),
+			];
+			$params[0] .= ($this->_premium_merchant ? ' = 1' : ' IS NULL') . ' GROUP BY a.id';
+			$coupon     = $this->db->fetchOne(array_shift($params), Db::FETCH_OBJ, $params);
+			if (!$coupon || ($coupon->multiple_use && $coupon->usage > 1)) {
+				throw new Error("Kode voucher {$code} tidak valid! Silahkan cek lagi atau kosongkan untuk melanjutkan pemesanan.");
+			}
 			foreach ($this->_server->orders as $merchant_id => $order) {
-				if (!$order->coupon_code) {
-					continue;
-				}
 				$params = [<<<QUERY
 					SELECT
 						a.id,
@@ -39,56 +65,23 @@ QUERY
 				$params[0] .= ($this->_premium_merchant ? ' = 1' : ' IS NULL');
 				$merchant   = $this->db->fetchOne(array_shift($params), Db::FETCH_OBJ, $params);
 				if (!$merchant) {
-					throw new Error("Kode voucher {$order->coupon_code} tidak valid! Silahkan cek lagi atau kosongkan untuk melanjutkan pemesanan.");
+					throw new Error("Kode voucher {$code} tidak valid! Silahkan cek lagi atau kosongkan untuk melanjutkan pemesanan.");
 				}
-				$params = [<<<QUERY
-					SELECT
-						a.price_discount,
-						a.discount_type,
-						a.multiple_use,
-						a.minimum_purchase,
-						COUNT(1) AS usage
-					FROM
-						coupons a
-						LEFT JOIN orders b ON a.id = b.coupon_id AND b.status = '1' AND b.buyer_id = ?
-					WHERE
-						a.status = '1' AND
-						a.effective_date <= ? AND
-						a.expiry_date > ? AND
-						LOWER(a.code) = ? AND
-						a.user_id
-QUERY
-					,
-					$this->_current_user->id,
-					$today,
-					$today,
-					strtolower($order->coupon_code),
-				];
-				$params[0] .= ($this->_premium_merchant ? ' = 1' : ' IS NULL') . ' GROUP BY a.id';
-				$coupon     = $this->db->fetchOne(array_shift($params), Db::FETCH_OBJ, $params);
-				if (!$coupon || ($coupon->multiple_use && $coupon->usage > 1)) {
-					throw new Error("Kode voucher {$order->coupon_code} tidak valid! Silahkan cek lagi atau kosongkan untuk melanjutkan pemesanan.");
-				}
-				$original_bill = 0;
-				$order_items = $this->_server->items[$merchant->id];
-				print_r($order_items);
+				$total = 0;
 				foreach ($order->items as $item) {
 					$product = $this->db->fetchOne('SELECT price, stock FROM products WHERE published = 1 AND price > 0 AND stock > 0 AND user_id = ? AND id = ?', Db::FETCH_OBJ, [$merchant->id, $item->product_id]);
 					if (!$product) {
 						throw new Error("Produk {$merchant->company} tidak valid! Silahkan cek pesanan Anda kembali atau hapus pesanan dari penjual tersebut untuk melanjutkan pemesanan!");
 					}
-					$original_bill += $product->price * min($item->quantity, $product->stock);
+					$total += $product->price * min($item->quantity, $product->stock);
 				}
-				if ($coupon->minimum_purchase && $original_bill < $coupon->minimum_purchase) {
-					throw new Error("Kode voucher {$order->coupon_code} berlaku jika belanja minimal Rp. " . number_format($coupon->minimum_purchase));
+				if ($coupon->minimum_purchase && $total < $coupon->minimum_purchase) {
+					throw new Error("Kode voucher {$code} berlaku untuk belanja minimal Rp. " . number_format($coupon->minimum_purchase));
 				}
-				$discount = $coupon->discount_type == 1 ? $coupon->price_discount : ceil($coupon->price_discount * $original_bill / 100.0);
-				if ($discount) {
-					$discounts[] = ['merchant_id' => $merchant->id, 'discount' => $discount];
-				}
+				$discount = $coupon->discount_type == 1 ? $coupon->price_discount : ceil($coupon->price_discount * $total / 100.0);
 			}
-			$this->_response['status']            = 1;
-			$this->_response['data']['discounts'] = $discounts;
+			$this->_response['status']           = 1;
+			$this->_response['data']['discount'] = $discount;
 		} catch (Error $e) {
 			$this->_response['message'] = $e->getMessage();
 		}
