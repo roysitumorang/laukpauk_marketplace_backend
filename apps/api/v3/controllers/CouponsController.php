f588165b32ce6d54ce5f566837a2bbf2797faa64
Roy Situmorang <roy.situmorang@gmail.com>
2017-06-21 18:11:57 +0700

    Fix coupons API v3.

--- apps/api/v3/controllers/CouponsController.php
+++ apps/api/v3/controllers/CouponsController.php
@@ -3,8 +3,8 @@
 namespace Application\Api\V3\Controllers;
 
 use Application\Models\Role;
-use Error;
 use Phalcon\Db;
+use Phalcon\Exception;
 
 class CouponsController extends ControllerBase {
 	function checkAction() {
@@ -13,7 +13,7 @@ class CouponsController extends ControllerBase {
 		$today = $this->currentDatetime->format('Y-m-d');
 		try {
 			if (!$this->request->isOptions() || $this->_current_user->role->name != 'Buyer' || !$code) {
-				throw new Error('Request tidak valid!');
+				throw new Exception('Request tidak valid!');
 			}
 			$params = [<<<QUERY
 				SELECT
@@ -29,19 +29,19 @@ class CouponsController extends ControllerBase {
 					a.status = '1' AND
 					a.effective_date <= ? AND
 					a.expiry_date > ? AND
-					LOWER(a.code) = ? AND
+					a.code = ? AND
 					a.user_id
 QUERY
 				,
 				$this->_current_user->id,
 				$today,
 				$today,
-				strtolower($code),
+				$code,
 			];
 			$params[0] .= ($this->_premium_merchant ? ' = 1' : ' IS NULL') . ' GROUP BY a.id';
 			$coupon     = $this->db->fetchOne(array_shift($params), Db::FETCH_OBJ, $params);
 			if (!$coupon || ($coupon->multiple_use && $coupon->usage > 1)) {
-				throw new Error("Kode voucher {$code} tidak valid! Silahkan cek lagi atau kosongkan untuk melanjutkan pemesanan.");
+				throw new Exception("Kode voucher {$code} tidak valid! Silahkan cek lagi atau kosongkan untuk melanjutkan pemesanan.");
 			}
 			foreach ($this->_server->orders as $order) {
 				$params = [<<<QUERY
@@ -52,7 +52,7 @@ QUERY
 						COALESCE(b.minimum_purchase, a.minimum_purchase, c.value::int) AS minimum_purchase
 					FROM
 						users a
-						JOIN service_areas b ON a.id = b.user_id
+						JOIN coverage_area b ON a.id = b.user_id
 						JOIN settings c ON c.name = 'minimum_purchase'
 					WHERE
 						a.status = 1 AND
@@ -65,22 +65,22 @@ QUERY
 				$params[0] .= ($this->_premium_merchant ? ' = 1' : ' IS NULL');
 				$merchant   = $this->db->fetchOne(array_shift($params), Db::FETCH_OBJ, $params);
 				if (!$merchant) {
-					throw new Error("Kode voucher {$code} tidak valid! Silahkan cek lagi atau kosongkan untuk melanjutkan pemesanan.");
+					throw new Exception("Kode voucher {$code} tidak valid! Silahkan cek lagi atau kosongkan untuk melanjutkan pemesanan.");
 				}
 				foreach ($order->products as $item) {
-					$product = $this->db->fetchOne('SELECT price, stock FROM products WHERE published = 1 AND price > 0 AND stock > 0 AND user_id = ? AND id = ?', Db::FETCH_OBJ, [$merchant->id, $item->id]);
+					$product = $this->db->fetchOne('SELECT price, stock FROM user_product WHERE published = 1 AND price > 0 AND stock > 0 AND user_id = ? AND id = ?', Db::FETCH_OBJ, [$merchant->id, $item->id]);
 					if (!$product) {
-						throw new Error("Produk {$merchant->company} tidak valid! Silahkan cek pesanan Anda kembali atau hapus pesanan dari penjual tersebut untuk melanjutkan pemesanan!");
+						throw new Exception("Produk {$merchant->company} tidak valid! Silahkan cek pesanan Anda kembali atau hapus pesanan dari penjual tersebut untuk melanjutkan pemesanan!");
 					}
 					$total += $product->price * min($item->quantity, $product->stock);
 				}
 			}
 			if ($coupon->minimum_purchase && $total < $coupon->minimum_purchase) {
-				throw new Error("Kode voucher {$code} berlaku untuk belanja minimal Rp. " . number_format($coupon->minimum_purchase));
+				throw new Exception("Kode voucher {$code} berlaku untuk belanja minimal Rp. " . number_format($coupon->minimum_purchase));
 			}
 			$this->_response['status']           = 1;
 			$this->_response['data']['discount'] = $coupon->discount_type == 1 ? $coupon->price_discount : ceil($coupon->price_discount * $total / 100.0);
-		} catch (Error $e) {
+		} catch (Exception $e) {
 			$this->_response['message'] = $e->getMessage();
 		}
 		$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
