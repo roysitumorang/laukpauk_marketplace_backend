b96f924014de164e14ad3bf81cb3ac50ca9c9f9a
Roy Situmorang <roy.situmorang@gmail.com>
2017-06-26 01:54:52 +0700

    Refactor posts API v3.

--- apps/api/v3/merchant/PostsController.php
+++ apps/api/v3/merchant/PostsController.php
@@ -3,7 +3,7 @@
 namespace Application\Api\V3\Merchant;
 
 use Application\Models\PostCategory;
-use Exception;
+use Phalcon\Exception;
 
 class PostsController extends ControllerBase {
 	function beforeExecuteRoute() {}
@@ -18,15 +18,16 @@ class PostsController extends ControllerBase {
 			if (!$post) {
 				throw new Exception('Konten tidak ditemukan!');
 			}
-			$this->_response['status']       = 1;
-			$this->_response['data']['post'] = [
-				'subject' => $post->subject,
-				'body'    => str_replace("\r\n", '', $post->body),
+			$this->_response = [
+				'status' => 1,
+				'data'   => [
+					'post' => ['subject' => $post->subject, 'body' => strtr($post->body, ["\r\n" => ''])],
+				],
 			];
 		} catch (Exception $e) {
 			$this->_response['message'] = $e->getMessage();
 		}
-		$this->response->setJsonContent($this->_response, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
+		$this->response->setJsonContent($this->_response, JSON_UNESCAPED_SLASHES);
 		return $this->response;
 	}
 }
